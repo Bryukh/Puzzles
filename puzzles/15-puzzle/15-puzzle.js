1b9a18856ca4e2cca6c8e0a9c5c0ff18ff59be9b
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-10-27 10:54:45 +0300

    Solve Snap problem with container and import

--- puzzles/15-puzzle/15-puzzle.js
+++ puzzles/15-puzzle/15-puzzle.js
@@ -1,3 +1,29 @@
-import Snap from "snapsvg";
+import Snap from 'snapsvg';
+import $ from 'jquery';
 
-console.log(Snap);
+class Puzzle {
+    constructor(containerId, options) {
+        this.container = $("#" + containerId);
+        this.options = options || {};
+    }
+
+    create() {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        this.container.append($(svg).attr("id", "puzzle_svg"));
+        var s = Snap("#puzzle_svg");
+        s.attr({width: "300px", height: "200px"});
+        var bigCircle = s.circle(150, 150, 100);
+        bigCircle.attr({
+            fill: "#bada55",
+            stroke: "#000",
+            strokeWidth: 5
+        });
+    }
+
+}
+
+var p = new Puzzle('puzzle');
+p.create();
+
+
+export default Puzzle;
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ module.exports = {
 
     entry: { "15-puzzle": "./puzzles/15-puzzle/15-puzzle.js"},
     output: { filename: "build/[name]-bundle.js" },
-    plugins: [ new webpack.optimize.CommonsChunkPlugin("build/init.js") ],
+    plugins: [ new webpack.optimize.CommonsChunkPlugin("init", "build/init.js", Infinity) ],
     module: {
         loaders: [
+            {
+                test: require.resolve('snapsvg'),
+                loader: 'imports-loader?this=>window,fix=>module.exports=0'
+            },
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
